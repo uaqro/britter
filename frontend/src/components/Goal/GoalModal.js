ffb61840327dceb58e9585157cda91e228396f2f
Uli <uaqro@protonmail.com>
2020-02-29 14:38:05 -0600

    working on goal modal

--- frontend/src/components/Goal/GoalModal.js
+++ frontend/src/components/Goal/GoalModal.js
@@ -1,23 +1,53 @@
 import MY_SERVICE from "../services";
 import React, { useState } from "react";
+import GoalModalObj from "./GoalModalObj";
+import GoalModalSettings from "./GoalModalSettings";
 
-const MailModal = ({ show, handleClose, producto }) => {
+const GoalModal = ({ show, handleClose }) => {
+  const handleShow = show ? "modal display-block" : "modal display-none";
 
-<form onSubmit={() => MY_SERVICE.newGoal(goal)}>
-          <input
-            type="text"
-            name="object"
-            onChange={evt => handleInputs(evt)}
-            onBlur={evt => checkBudget(evt.target.value)}
-          />
-          <input
-            type="number"
-            name="goal"
-            onChange={evt => handleInputs(evt)}
-          />
-          <input
-            type="number"
-            name="daysToGoal"
-            onChange={evt => handleInputs(evt)}
-          />
-        </form>
+  const [goal, setGoal] = useState({ daysToGoal: 0, object: "", goal: 0 });
+  const [priceRecommendation, setPriceRecommendation] = useState(0);
+  const [creditValues, setCreditValues] = useState({
+    interest: "",
+    enganche: "",
+    appliesForCredit: false
+  });
+  const [step, setStep] = useState(true);
+  const handleInputs = evt =>
+    setGoal({ ...goal, [evt.target.name]: evt.target.value });
+  const checkBudget = val => {
+    const recommendation = MY_SERVICE.checkGoalObject(val);
+    setPriceRecommendation(recommendation.price);
+    appliesForCredit
+      ? setCreditValues({
+          appliesForCredit: true,
+          interest: recommendation.interest,
+          enganche: recommendation.enganche
+        })
+      : null;
+  };
+  return (
+    <div className={handleShow}>
+      {step ? (
+        <GoalModalObj
+          stepping={setStep}
+          checkGoal={checkGoal}
+          handleInputs={handleInputs}
+          checkBudget={checkBudget}
+          goal={goal}
+          handleClose={handleClose}
+        />
+      ) : (
+        <GoalModalSettings
+          credit={creditValues}
+          price={priceRecommendation}
+          handleInputs={handleInputs}
+          handleClose={handleClose}
+        />
+      )}
+    </div>
+  );
+};
+
+export default GoalModal;
--- frontend/src/components/Goal/Goals.js
+++ frontend/src/components/Goal/Goals.js
@@ -3,30 +3,17 @@ import MY_SERVICE from "../../services/index";
 import styled from "styled-components";
 
 const Goals = props => {
-  const [goal, setGoal] = useState({ daysToGoal: 0, object: "", goal: 0 });
-  const [priceRecommendation, setPriceRecommendation] = useState(0);
-  const [creditValues, setCreditValues] = useState({
-    interest: "",
-    enganche: ""
-  });
-  const handleInputs = evt =>
-    setGoal({ ...goal, [evt.target.name]: evt.target.value });
-  const checkBudget = val => {
-    const recommendation = MY_SERVICE.checkGoalObject(val);
-    setPriceRecommendation(recommendation.price);
-    appliesForCredit
-      ? setCreditValues({
-          interest: recommendation.interest,
-          enganche: recommendation.enganche
-        })
-      : null;
-  };
+  const [handleModal, setModal] = useState(false);
   return (
     <GoalsLayout>
       {ctx.user.spendGoals.map(element => (
         <GoalCard goal={element} />
       ))}
-      {ctx.user.spendGoals.length < 3 ? <GoalModal /> : <></>}
+      {ctx.user.spendGoals.length < 3 ? (
+        <GoalModal show={handlemodal} handle={setModal} />
+      ) : (
+        <></>
+      )}
     </GoalsLayout>
   );
 };
