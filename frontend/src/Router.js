3f2f0fdcd96dceabd77e0ef695852b96b4abc977
roganoalien <hola@rodrigogarcia.com.mx>
2020-02-29 21:07:52 -0600

    :zap: Husky
    Added to prevent bad commits

--- frontend/src/Router.js
+++ frontend/src/Router.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Home from './pages/home/Home';
+// import Home from './pages/home/Home';
 import Concierge from './pages/concierge/Concierge';
+// import UserProfile from './pages/profile/UserProfile';
 // import NotFound from './components/404/NotFound.js';
 
 const Router = () => (
@@ -9,6 +10,7 @@ const Router = () => (
 		<Switch>
 			{/* <Route exact path="/" component={Home} /> */}
 			<Route exact path="/con" component={Concierge} />
+			{/* <Route exact path="/profile" component={UserProfile} /> */}
 			{/* <Route component={NotFound} /> */}
 		</Switch>
 	</BrowserRouter>
