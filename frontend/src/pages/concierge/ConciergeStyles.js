c79e750cdd0336c41fcff3fca2734bfb749d0f27
roganoalien <hola@rodrigogarcia.com.mx>
2020-03-01 01:29:50 -0600

    :tada: Search Style Fixed
    Added the styles for the search view

--- frontend/src/pages/concierge/ConciergeStyles.js
+++ frontend/src/pages/concierge/ConciergeStyles.js
@@ -1,16 +1,47 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const ConciergeStyles = styled.div`
-
-  .card{
-    height:70px;
-    width:280px;
-    background:white;
-    position: absolute;
-    left: 50%;
-    top: 100px;
-    box-shadow: 0 10px 15px 10px rgba(0,0,0,0.25);
-    transform: translate(-50%, 0);
-    z-index: 10;
-  }
+	.main-logo {
+		left: 50%;
+		position: absolute;
+		top: 30px;
+		transform: translate(-50%, 0);
+		z-index: 10;
+	}
+	.card {
+		width: 280px;
+		background: white;
+		border: solid 1px #dddfe6;
+		border-radius: 4px;
+		padding: 15px;
+		position: absolute;
+		left: 50%;
+		top: 100px;
+		box-shadow: 0 5px 10px 12px rgba(37, 44, 65, 0.05);
+		transform: translate(-50%, 0);
+		width: calc(100% - 80px);
+		z-index: 10;
+	}
+	.mapGo {
+		right: 15px;
+		position: absolute;
+		top: 18px;
+	}
+	form input {
+		border: none;
+		color: #252c41;
+		font-size: 16px;
+		font-weight: bold;
+	}
+	form button {
+		opacity: 0;
+	}
+	.search p {
+		color: #252c41;
+		font-size: 14px;
+		font-weight: bold;
+		margin: 5px 0 0 2px;
+		opacity: 0.4;
+		text-align: left;
+	}
 `;
