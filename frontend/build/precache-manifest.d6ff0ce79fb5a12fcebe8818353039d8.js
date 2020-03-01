94234061bf63e6887c1ac66549faa28240c42bb1
EChColorado <edxz7c@gmail.com>
2020-03-01 10:23:41 -0600

    buld folder

---
+++ frontend/build/precache-manifest.d6ff0ce79fb5a12fcebe8818353039d8.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3ddb960ba5191423ade8ff718a70577",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5262590805af7485dc8",
+    "url": "/static/css/2.57fe0620.chunk.css"
+  },
+  {
+    "revision": "b2ccf3deee65d6aeae2d",
+    "url": "/static/css/main.f1dc5a45.chunk.css"
+  },
+  {
+    "revision": "f5262590805af7485dc8",
+    "url": "/static/js/2.a1e88439.chunk.js"
+  },
+  {
+    "revision": "dd15d96fa9f0d57ebf160b8596edc348",
+    "url": "/static/js/2.a1e88439.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2ccf3deee65d6aeae2d",
+    "url": "/static/js/main.5b368142.chunk.js"
+  },
+  {
+    "revision": "ccf1d8c0179706a077aa",
+    "url": "/static/js/runtime-main.3a02ae19.js"
+  },
+  {
+    "revision": "888c5de8ea3298adb111eaae551a6e36",
+    "url": "/static/media/britte-r.888c5de8.svg"
+  },
+  {
+    "revision": "888c5de8ea3298adb111eaae551a6e36",
+    "url": "/static/media/britter-logo.888c5de8.svg"
+  },
+  {
+    "revision": "2cab99b53d1d4e98df680d9bc32246d7",
+    "url": "/static/media/coin-gray.2cab99b5.svg"
+  },
+  {
+    "revision": "4bd5a897078cc5f526ee296b818caada",
+    "url": "/static/media/coin.4bd5a897.svg"
+  },
+  {
+    "revision": "d0e15065b65372cb411b11a06c2534d9",
+    "url": "/static/media/heart-beat.d0e15065.svg"
+  },
+  {
+    "revision": "6c1775d93dc8d73937d051a95ef2a50f",
+    "url": "/static/media/map-go.6c1775d9.svg"
+  },
+  {
+    "revision": "c8ee403ca5288cfab06b181700bd6914",
+    "url": "/static/media/user-default.c8ee403c.jpg"
+  }
+]);
