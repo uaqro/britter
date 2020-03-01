09863406dcd05dd683f3aeaa09da19d82622ab84
roganoalien <hola@rodrigogarcia.com.mx>
2020-03-01 01:08:21 -0600

    :zap: Now is fixed
    Conflicts and errors fixed

--- frontend/src/services/index.js
+++ frontend/src/services/index.js
@@ -1,55 +1,54 @@
-import axios from "axios";
+import axios from 'axios';
 let baseURL;
 
-process.env.NODE_ENV === "production"
-  ? (baseURL = "here should be your production endpoint")
-  : (baseURL = "http://localhost:3000");
+process.env.NODE_ENV === 'production'
+	? (baseURL = 'here should be your production endpoint')
+	: (baseURL = 'http://localhost:3000');
 
 const serviceNode = axios.create({ withCredentials: true, baseURL });
 const serviceFlask = axios.create({
-  withCredentials: true,
-  baseURL: "FLASK ENDPOINT"
+	withCredentials: true,
+	baseURL: 'FLASK ENDPOINT'
 });
 
 const MY_SERVICE = {
-  test: async () => {
-    return await serviceNode.get("/");
-  },
-  signup: async user => {
-    return await serviceNode.post("/signup", user);
-  },
-  login: async user => {
-    return await serviceNode.post("/login", user);
-  },
-  logOut: async () => {
-    return await serviceNode.get("/logout");
-  },
-  isLogged: async () => {
-    return await serviceNode.get("/loggedin");
-  },
-  congratz: async () => {
-    return await serviceNode.post("/congratz");
-  },
-  getUser: async () => {
-    return await service.get('/loggedin');
-  }
-  ,
-  recommendations: async frm => {
-    return await serviceFlask.post("/get-recommendations", frm);
-  },
-  location: async location => {
-    return await serviceFlask.post("/loc", location);
-    // return await serviceFlask.post("/get-recommendations", frm);
-  },
-  newGoal: async gls => {
-    return await serviceNode.post("/new-goal", gls);
-  },
-  checkGoalObject: async obj => {
-    return await serviceFlask.post("/check", obj);
-  },
-  updateGoal: async (gl, sv) => {
-    return await serviceNode.post("/update-goal", { goal: gl, sav: sv });
-  }
+	test: async () => {
+		return await serviceNode.get('/');
+	},
+	signup: async user => {
+		return await serviceNode.post('/signup', user);
+	},
+	login: async user => {
+		return await serviceNode.post('/login', user);
+	},
+	logOut: async () => {
+		return await serviceNode.get('/logout');
+	},
+	isLogged: async () => {
+		return await serviceNode.get('/loggedin');
+	},
+	congratz: async () => {
+		return await serviceNode.post('/congratz');
+	},
+	getUser: async () => {
+		return await serviceNode.get('/loggedin');
+	},
+	recommendations: async frm => {
+		return await serviceFlask.post('/get-recommendations', frm);
+	},
+	location: async location => {
+		return await serviceFlask.post('/loc', location);
+		// return await serviceFlask.post("/get-recommendations", frm);
+	},
+	newGoal: async gls => {
+		return await serviceNode.post('/new-goal', gls);
+	},
+	checkGoalObject: async obj => {
+		return await serviceFlask.post('/check', obj);
+	},
+	updateGoal: async (gl, sv) => {
+		return await serviceNode.post('/update-goal', { goal: gl, sav: sv });
+	}
 };
 
 export default MY_SERVICE;
